6068b252c042347c35bcbe84280d868f43f89273
David Pereira <david.pereira@dmfec.local>
2019-06-25 12:50:44 -0300

    Creating properties in components

--- ch02/hello-world-class/js/script.js
+++ ch02/hello-world-class/js/script.js
@@ -3,7 +3,11 @@ let h1 = React.createElement('h1', null, 'Hello world!')
 
 class HelloWorld extends React.Component {
     render() {
-        return React.createElement('div', null, h1, h1)
+        return React.createElement(
+            'h1', 
+            this.props, 
+            `Hello ${this.props.frameworkName} World!!!`
+        )
     }
 }
 
